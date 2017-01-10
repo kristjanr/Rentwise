aae6a3a6bfc921d5de76a1d92264a38516da2786
kristjanr <kristjan.r@gmail.com>
2017-01-10 21:07:25 +0200

    footer

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from app.views import ImageUploadView, ItemView, ItemDetailView, ItemDeleteView, publish_item, \
     unpublish_item, logout_view, home
@@ -12,4 +13,5 @@ urlpatterns = [
     url(r'^item/(?P<pk>[0-9]+)/publish', publish_item, name='publish_item'),
     url(r'^item/(?P<pk>[0-9]+)/unpublish', unpublish_item, name='unpublish_item'),
     url(r'^logout/facebook', logout_view, name='logout'),
+    url(r'^terms$', TemplateView.as_view(template_name='app/terms.html'), name='terms'),
 ]
