d731adfe6246c1b9b65cc4820880138d7c0e41d3
kristjanr <kristjan.r@gmail.com>
2017-01-11 11:55:54 +0200

    add all models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from app.models import Item, Profile
+from app.models import Item, Profile, Category, Image
 
 admin.site.register(Profile)
+admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(Image)
