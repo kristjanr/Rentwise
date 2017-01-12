1842d7bebd83587b58a93134b143dd3cf4d8e623
kristjanr <kristjan.r@gmail.com>
2017-01-12 12:26:05 +0200

    Add proper category names

--- app/models.py
+++ app/models.py
@@ -31,7 +31,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
